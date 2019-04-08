fbbd21b38fe42cbc9463518cd63e6d79ff8f56e5
Félix Sánchez-Tembleque <felixstdp@gmail.com>
2019-04-08 11:43:50 +0200

    Update basico.ino

--- scale_1_5/arduinomega/basico.ino
+++ scale_1_5/arduinomega/basico.ino
@@ -2,7 +2,7 @@ void setup() {
   for (i=[2:7]){
     pinMode(i,OUTPUT);
   }
-for (i=[31:43]){
+for (i=[30:42]){
     pinMode(i,OUTPUT);
   }
 
@@ -15,11 +15,11 @@ void loop() {
 
 void setmotor(int m, int v){
   int pwm = 8-i;
-  int aia = 28+2*i;
-  int aib = 29+2*i;
+  int dir1 = 28+2*i;
+  int dir2 = 29+2*i;
   v = contrain (v,-255,255);
-  digitalWrite(aia,v>=0);
-  digitalWrite(aib,v<0);
+  digitalWrite(dir1,v>=0);
+  digitalWrite(dir2,v<0);
   analogWrite(pwm,abs(a))
   }
 }
