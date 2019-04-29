10a37011ecf45b815a7ed91fb994b0eb90cc75ce
Félix Sánchez-Tembleque <felixstdp@gmail.com>
2019-04-29 20:39:41 +0200

    Update basico.ino

--- scale_1_5/arduinomega/basico.ino
+++ scale_1_5/arduinomega/basico.ino
@@ -6,42 +6,81 @@
 #define s4center 90
 
 Servo servo1;
-Servo servo2;
 Servo servo3;
 Servo servo4;
+Servo servo6;
 
-void setup() {
+void setup()
+{
 
-  for (i=[2:7]){
+  for (int i = 2; i <= 7; i++)
+  {
     pinMode(i,OUTPUT); //pines PWM para velocidad
   }
-  for (i=[30:42]){
+  for (int i = 30; i <= 42; i++)
+  {
     pinMode(i,OUTPUT); //pines digitales para control de dirección
   }
   
-  servo1.attach(8); //pines para servos
-  servo2.attach(9);
-  servo3.attach(10);
-  servo4.attach(11);
-
+  servo1.attach(10); //pines para servos
+  servo3.attach(11);
+  servo4.attach(8);
+  servo6.attach(9);
+  
   servo1.write(s1center); //arranque en posicion central
-  servo2.write(s2center);
-  servo3.write(s3center);
-  servo4.write(s4center);
+  servo3.write(s2center);
+  servo4.write(s3center);
+  servo6.write(s4center);
 
 }
 
-void loop() {
-
+void loop()
+{
+  forward();
+  delay(2000);
+  reverse();
+  delay(2000);
+  brake();
+  delay(2000);
 }
 
-void setmotor(int m, int v){
-  int pwm = 8-i;
-  int dir1 = 28+2*i;
-  int dir2 = 29+2*i;
+void setmotor(int m, int v)
+{
+  int pwm = 8-m;
+  int dir1 = 28+2*m;
+  int dir2 = 29+2*m;
   v = constrain(v,-255,255);
-  digitalWrite(dir1,v>=0);
-  digitalWrite(dir2,v<0);
+  digitalWrite(dir1,v<0);
+  digitalWrite(dir2,v>=0);
   analogWrite(pwm,abs(v));
-  }
+}
+
+void forward()
+{
+  setmotor(1,128);
+  setmotor(2,128);
+  setmotor(3,128);
+  setmotor(4,128);
+  setmotor(5,128);
+  setmotor(6,128);
+}
+
+void reverse()
+{
+  setmotor(1,-128);
+  setmotor(2,-128);
+  setmotor(3,-128);
+  setmotor(4,-128);
+  setmotor(5,-128);
+  setmotor(6,-128);
+}
+
+void brake()
+{
+setmotor(1,0);
+setmotor(2,0);
+setmotor(3,0);
+setmotor(4,0);
+setmotor(5,0);
+setmotor(6,0);
 }
