6049d02479f9ba8d8c7705142020a78b7299e57c
Félix Sánchez-Tembleque <felixstdp@gmail.com>
2019-10-06 13:19:13 +0200

    Create emisora_curiosity.ino

---
+++ scale_1_5/arduinomega/emisora_curiosity.ino
@@ -0,0 +1,35 @@
+#define chnum 8 // numero de canales de la emisora
+int chval[chnum]; // valores de los canales
+int PPMpin = 4; // pin de entrada PPM
+
+void setup()
+{
+ Serial.begin(9600); 
+ pinMode(PPMpin, INPUT); 
+}
+
+void loop()
+{
+  chval[chnum] = readppm();
+  
+  for(int i = 0; i < chnum; i++) 
+  {
+    Serial.print(chval[i]);
+    Serial.print("\t");
+  }
+  Serial.println();
+  delay(200);  
+}
+
+int readppm()
+{
+  while(pulseIn(PPMpin, HIGH, 10000)<2000){} //espera al canal 0
+  
+  for(int i=0; i<chnum; i++)
+  {
+    chval[i]=pulseIn(PPMpin, HIGH, 10000);
+  }
+  
+  // devuleve el resultado
+  return chval[chnum];
+}  
